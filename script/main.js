5df21d166a97df5d0df6bbe27e38eb0158508216
Sean Moriarty <hippiehaircut@gmail.com>
2021-06-20 16:59:46 -0700

    added onclick handlers for buttons

---
+++ script/main.js
@@ -0,0 +1,29 @@
+topLeftBtn.onclick = () => {
+    console.log("top left");
+};
+topCenterBtn.onclick = () => {
+    console.log("top center");
+};
+topRightBtn.onclick = () => {
+    console.log("top right");
+};
+midLeftBtn.onclick = () => {
+    console.log("mid left");
+};
+midCenterBtn.onclick = () => {
+    console.log("mid center");
+};
+midRightBtn.onclick = () => {
+    console.log("mid right");
+};
+bottomLeftBtn.onclick = () => {
+    console.log("bottom left");
+};
+bottomCenterBtn.onclick = () => {
+    console.log("bottom center");
+};
+bottomRightBtn.onclick = () => {
+    console.log("bottom right");
+};
+
+
